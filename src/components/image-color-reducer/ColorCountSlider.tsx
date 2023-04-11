ee68f1151bed6287af5b9889bdd40f3297f35a20
Suiko <suiko@riseup.net>
2023-04-11 08:24:28 +0800

    style(components): formatting

--- src/components/image-color-reducer/ColorCountSlider.tsx
+++ src/components/image-color-reducer/ColorCountSlider.tsx
@@ -14,20 +14,23 @@ const ColorCountSlider: React.FC<ColorCountSliderProps> = (props) => {
         Current Color Count: <span className="font-extrabold">{value}</span>
       </span>
       <form>
-      <Slider.Root
-        className="center relative flex h-8 w-full touch-none select-none items-center"
-        value={[value]}
-        onValueChange={(changedValue) => onChange(changedValue[0] || 1)}
-        defaultValue={[32]}
-        max={100}
-        step={1}
-        aria-label="Color Count"
-      >
-        <Slider.Track className="relative h-2 flex-grow rounded-[9999px] bg-violet-950">
-          <Slider.Range className="absolute h-full rounded-[9999px] bg-white" />
-        </Slider.Track>
-        <Slider.Thumb className="block h-8 w-8 rounded-2xl bg-white" aria-label="Color Count handler" />
-      </Slider.Root>
+        <Slider.Root
+          className="center relative flex h-8 w-full touch-none select-none items-center"
+          value={[value]}
+          onValueChange={(changedValue) => onChange(changedValue[0] || 1)}
+          defaultValue={[32]}
+          max={100}
+          step={1}
+          aria-label="Color Count"
+        >
+          <Slider.Track className="relative h-2 flex-grow rounded-[9999px] bg-violet-950">
+            <Slider.Range className="absolute h-full rounded-[9999px] bg-white" />
+          </Slider.Track>
+          <Slider.Thumb
+            className="block h-8 w-8 rounded-2xl bg-white"
+            aria-label="Color Count handler"
+          />
+        </Slider.Root>
       </form>
     </>
   );
