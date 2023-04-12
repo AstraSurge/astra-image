6eb46f4e55cf1f40c9e7eae3e1e0e875daf15ae0
Suiko <suiko@riseup.net>
2023-04-12 08:51:03 +0800

    fix(image-color-reducer): fix incorrect hooks dependency

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
